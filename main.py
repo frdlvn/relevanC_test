6eda5b949c2af5ad76667a36fe0746fa45300561
Frédéric Lavner <frederic.lavner@gmail.com>
2021-03-22 15:10:36 +0000

    First modifications

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from pyspark.sql import SparkSession
-from pyspark.conf import SparkConf
 from pyspark.sql.dataframe import DataFrame
 import pyspark.sql.functions as F
 
@@ -15,17 +14,17 @@ start_time = time.time()
 spark = (SparkSession
     .builder
     .appName("relevanC test")
-    .config("spark.driver.memory", "17g")
+    #.config("spark.driver.memory", "17g")
     .getOrCreate()
 )
 
 # Load the dataframe from the file
-raw_df = spark.read.option("header", "true").option("sep", "|").csv("relevanC/randomized-transactions-202009.psv")
+raw_df = spark.read.option("header", "true").option("sep", "|").csv("randomized-transactions-202009.psv")
 
 # Adjust date and float types (default is string)
 df = (raw_df.withColumn("date_encaissement", F.to_date(F.col("date_encaissement")))
             .withColumn("prix", F.col("prix").cast("float"))
-            #.limit(10000)  # for tests only
+            .limit(10000)  # for tests only
             .cache()
 )
 
@@ -34,14 +33,13 @@ df.printSchema()
 
 #---------Top 50 stores-----------#
 top_50_stores_df: DataFrame = (df.select("code_magasin", "prix")
-                                 .groupBy("code_magasin").sum()
-                                 .withColumnRenamed("sum(prix)", "ca")
+                                 .groupBy("code_magasin").agg(sum("prix").alias("ca"))
                                  .orderBy("ca", ascending=False)
                                  .limit(50)
 )
 
 # Write dataframe to CSV file
-top_50_stores_df.write.save("relevanC/top-50-stores", header="true", format="csv", mode="overwrite", sep="|")
+top_50_stores_df.coalesce(1).write.save("top-50-stores", header="true", format="csv", mode="overwrite", sep="|")
 
 # Visual of the KPI
 sns.set_theme(style="whitegrid")
@@ -49,7 +47,7 @@ f, ax = plt.subplots(figsize=((12, 15)))
 sns.set_color_codes("pastel")
 sns_plot = sns.barplot(x="ca", y="code_magasin", data=top_50_stores_df.toPandas(), color="b")
 ax.set(ylabel="", xlabel="Chiffre d'affaire", title="Classement des 50 premiers magasins, par chiffre d'affaire")
-sns_plot.get_figure().savefig("relevanC/top-50-stores/top-50-stores.png")
+sns_plot.get_figure().savefig("top-50-stores/top-50-stores.png")
 plt.close()
 
 #---------Top 100 products-----------#
